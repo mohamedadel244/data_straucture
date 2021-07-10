7b82c7feb7ff6e148130dab110356dbb58fe7c01
mohamedadel244 <87205409+mohamedadel244@users.noreply.github.com>
2021-07-10 23:11:32 +0200

    Update main.c

--- main.c
+++ main.c
@@ -1,6 +1,17 @@
+//عمر ايمن عبد المتعال عبد الحليم
+//Sec 3 BN 13
+// رقم الجلوس 32124
+//محمد عادل كامل سيد
+//Sec 3 BN 39
+//رقم الجلوس 32150
+//هشام مدحت محمد طاهر
+//Sec 4 BN 41
+//رقم الجلوس 32207
+
 #include<stdlib.h>
 #include<string.h>
 #include<stdio.h>
+#include <time.h>
  
 struct student
     {
@@ -28,8 +39,7 @@ void linked_list()
   void read_data()
     {
         printf("Enter the name of the student \n");
-        fflush(stdin);
-        gets(a.name);
+        scanf("%s",a.name);
 
         printf("Enter the day of birth of the student \n");
         scanf("%d",&a.day);
@@ -107,14 +117,7 @@ void linked_list()
 
     void insert_middle (int l,struct student a)
     {
-        if (l<1)
-        {
-            printf("\nWrong location enter a valid number\n");
-        }
-        else if(l > list_count+1)
-            printf("\nWrong location total length of the list is %d\n",list_count);
-        else
-        {
+     
             struct node* temp = NULL;
             temp = (struct node*)malloc(sizeof (struct node));
             temp->data = a;
@@ -137,14 +140,73 @@ void linked_list()
             temp->next=aft;
             pre->next=temp;
             list_count++;
-        }
-        }
+        }  
     }
+	void size_time_complexitiy()
+{
+    printf("the size of struct student %d\n",sizeof(struct student));
+    printf("the size of struct node %d\n",sizeof(struct node));
+    int N,x;
+        printf("1. small N\n2. medium N\n3. large N\n");
+        printf("Enter your choice: ");
+        scanf("%d",&x);
+       
+        switch(x)
+        {
+            case 1:  N = 50;  break;
+
+            case 2:  N = 500; break;
+            
+            case 3:  N = 1200; break;
+            
+            default: printf("\nWrong selection!!!");
+        }
+    for (int k=0; k<N; k++)
+    {
+        struct student w1;
+        strcpy(w1.name,"Hesham medhat");
+        w1.id=100;
+        w1.day=5;
+        w1.month=12;
+        w1.year=2000;
+        w1.lyscore=90;
+        insert_end(w1);
+    }
+    struct student w2;
+        strcpy(w2.name,"omar ayman");
+        w2.id=100;
+        w2.day=5;
+        w2.month=12;
+        w2.year=2000;
+        w2.lyscore=90;
+        clock_t t1;
+        t1 = clock();
+        insert_beginning (w2);
+        t1 = clock() - t1;
+        double time_taken = ((double)t1)/CLOCKS_PER_SEC; // in seconds
+        printf("fun(insert_beginning) took %f seconds to execute \n", time_taken);
+        clock_t t2;
+        t2 = clock();
+        insert_end (w2);
+        t2 = clock() - t2;
+        double time_taken2 = ((double)t2)/CLOCKS_PER_SEC; // in seconds
+        printf("fun(insert_end) took %f seconds to execute \n", time_taken2);
+        int location = N/2;
+        clock_t t3;
+        t3 = clock();
+        insert_middle (location,w2);
+        t3 = clock() - t3;
+        double time_taken3 = ((double)t3)/CLOCKS_PER_SEC; // in seconds
+        printf("fun(insert_middle) took %f seconds to execute \n", time_taken3);
+
+}
+	
+	
 int choice,loc;
 while(1)
 {
     printf("\n\n**LIST MENU **\n");
-    printf("1. insert beginning\n2. insert end\n3. insert middle\n4. Exit\n");
+     printf("1. insert beginning\n2. insert end\n3. insert middle\n4. size and time complexitiy\n5. Exit\n");
     printf("Enter your choice: ");
     scanf("%d",&choice);
     switch(choice)
@@ -159,15 +221,28 @@ while(1)
                     printlist();
                     break;
 
-        case 3:     
-                    read_data();
-                    printf("Enter the location of the student \n");
+        case 3:     printf("Enter the location of the student \n");
                     scanf("%d",&loc);
+                    if (loc <1)
+                    {
+                    printf("\nWrong location enter a valid number\n");
+                    printlist();
+                    break;
+                    }
+                    else if(loc > list_count+1)
+                    {printf("\nWrong location total length of the array is %d\n",list_count);
+                    printlist();
+                    break;}
+                    read_data();
                     insert_middle (loc,a);
                     printlist();
                     break;
 
-        case 4:    return;
+        case 4:     size_time_complexitiy();
+                    break;
+
+        case 5:    return;
+
 
         default: printf("\nWrong selection!!! Please try again!!!\n");
  
@@ -185,8 +260,7 @@ void dynamic_array()
     void read_data()
     {
         printf("Enter the name of the student \n");
-        fflush(stdin);
-        gets(a.name);
+        scanf("%s",a.name);
 
         printf("Enter the day of birth of the student \n");
         scanf("%d",&a.day);
@@ -240,14 +314,6 @@ void dynamic_array()
 
     void insert_middle(struct student w,int loc)
     {
-        if (loc <1)
-        {
-            printf("\nWrong location enter a valid number\n");
-        }
-        else if(loc > n+1)
-            printf("\nWrong location total length of the array is %d\n",n);
-        else
-        {
             n++;
             realloc(darr,sizeof(struct student) * (n));
             int i;
@@ -257,13 +323,70 @@ void dynamic_array()
             }
             darr[i]=w;
 }
+	
+	void size_time_complexitiy()
+{
+    printf("the size of struct student %d\n",sizeof(struct student));
+    int N,x;
+        printf("1. small N\n2. medium N\n3. large N\n");
+        printf("Enter your choice: ");
+        scanf("%d",&x);
+       
+        switch(x)
+        {
+            case 1:  N = 50;  break;
+
+            case 2:  N = 500; break;
+            
+            case 3:  N = 1200; break;
+            
+            default: printf("\nWrong selection!!!");
+        }
+    for (int k=0; k<N; k++)
+    {
+        struct student w1;
+        strcpy(w1.name,"Hesham medhat");
+        w1.id=100;
+        w1.day=5;
+        w1.month=12;
+        w1.year=2000;
+        w1.lyscore=90;
+        insert_end(w1);
+    }
+    struct student w2;
+        strcpy(w2.name,"omar ayman");
+        w2.id=100;
+        w2.day=5;
+        w2.month=12;
+        w2.year=2000;
+        w2.lyscore=90;
+        clock_t t1;
+        t1 = clock();
+        insert_beginning (w2);
+        t1 = clock() - t1;
+        double time_taken = ((double)t1)/CLOCKS_PER_SEC; // in seconds
+        printf("fun(insert_beginning) took %f seconds to execute \n", time_taken);
+        clock_t t2;
+        t2 = clock();
+        insert_end (w2);
+        t2 = clock() - t2;
+        double time_taken2 = ((double)t2)/CLOCKS_PER_SEC; // in seconds
+        printf("fun(insert_end) took %f seconds to execute \n", time_taken2);
+        int location = N/2;
+        clock_t t3;
+        t3 = clock();
+        insert_middle (w2,location);
+        t3 = clock() - t3;
+        double time_taken3 = ((double)t3)/CLOCKS_PER_SEC; // in seconds
+        printf("fun(insert_middle) took %f seconds to execute \n", time_taken3);
+
 }
 
     int choice,loc;
      while(1)
      {
         printf("\n\n**Array MENU **\n");
-        printf("1. insert beginning\n2. insert end\n3. insert middle\n4. Exit\n");
+         printf("1. insert beginning\n2. insert end\n3. insert middle\n4. size and time complexitiy\n5. Exit\n");
         printf("Enter your choice: ");
         scanf("%d",&choice);
         switch(choice)
@@ -278,15 +401,27 @@ void dynamic_array()
                         printarray ();
                         break;
 
-            case 3:    
-                        read_data();
-                        printf("Enter the location of the student \n");
+            case 3:     printf("Enter the location of the student \n");
                         scanf("%d",&loc);
+                        if (loc <1)
+                        {
+                        printf("\nWrong location enter a valid number\n");
+                        printarray ();
+                        break;
+                        }
+                        else if(loc > n+1)
+                        {printf("\nWrong location total length of the array is %d\n",n);
+                        printarray ();
+                        break;}
+                        read_data();
                         insert_middle (a,loc);
                         printarray ();
                         break;
+                        
+            case 4:     size_time_complexitiy();
+                        break;
 
-            case 4:     return;
+            case 5:     return;
             default:    printf("\nWrong selection!!! Please try again!!!\n");
             }
     }
@@ -301,8 +436,7 @@ int main()
 
     while(1)
     {
-        printf("Welcome. Let's start the program");
-        printf("\n\n**MAIN MENU **\n");
+        printf("\n\n*MAIN MENU *\n");
         printf("1. linked list\n2. dynamic array\n3. Exit\n");
         printf("Enter your choice: ");
         scanf("%d",&choice);
